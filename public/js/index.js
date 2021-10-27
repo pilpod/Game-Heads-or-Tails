bb99636c71995a7bfa2cd405e1a6f0c834d55174
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2021-10-27 15:28:34 +0200

    pending fix coin bug when button is clicked

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('hello typescript');
+var btnStart = document.getElementById('btn-start');
+var btnReset = document.getElementById('btn-reset');
+var coinBox = document.getElementById('coin');
+btnStart.addEventListener('click', function (e) {
+    e.preventDefault();
+    var headsOrTails = Math.floor(Math.random() * 2);
+    if (headsOrTails === 0) {
+        coinBox.className = 'tails';
+        console.log(coinBox.className);
+    }
+    if (headsOrTails === 1) {
+        coinBox.className = 'heads';
+        console.log(coinBox.className);
+    }
+});
+btnReset.addEventListener('click', function () {
+    coinBox.className = '';
+});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,23 @@
-console.log('hello typescript');
+const btnStart = document.getElementById('btn-start') as HTMLButtonElement;
+const btnReset = document.getElementById('btn-reset') as HTMLButtonElement;
+let coinBox = document.getElementById('coin') as HTMLDivElement;
+
+btnStart.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let headsOrTails = Math.floor(Math.random() * 2);
+    
+    if(headsOrTails === 0) {
+        coinBox.className = 'tails';
+        console.log(coinBox.className)
+    }
+
+    if(headsOrTails === 1) {
+        coinBox.className = 'heads';
+        console.log(coinBox.className)
+    }
+})
+
+btnReset.addEventListener('click', () => {
+    coinBox.className = '';
+})
