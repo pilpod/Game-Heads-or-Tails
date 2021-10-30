1be5c34801b119a4d02b9e24dea52cf477adc7e8
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2021-10-30 14:30:57 +0200

    bug fixed game working

--- public/js/index.js
+++ public/js/index.js
@@ -4,15 +4,21 @@ var btnReset = document.getElementById('btn-reset');
 var coinBox = document.getElementById('coin');
 btnStart.addEventListener('click', function (e) {
     e.preventDefault();
+    var currenClass = '';
     var headsOrTails = Math.floor(Math.random() * 2);
     if (headsOrTails === 0) {
         coinBox.className = 'tails';
+        currenClass = 'tails';
         console.log(coinBox.className);
     }
     if (headsOrTails === 1) {
         coinBox.className = 'heads';
+        currenClass = 'heads';
         console.log(coinBox.className);
     }
+    setTimeout(function () {
+        coinBox.classList.remove(currenClass);
+    }, 5000);
 });
 btnReset.addEventListener('click', function () {
     coinBox.className = '';
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,17 +5,25 @@ let coinBox = document.getElementById('coin') as HTMLDivElement;
 btnStart.addEventListener('click', (e) => {
     e.preventDefault();
 
+    let currenClass:string = '';
+    
     let headsOrTails = Math.floor(Math.random() * 2);
     
     if(headsOrTails === 0) {
         coinBox.className = 'tails';
+        currenClass = 'tails';
         console.log(coinBox.className)
     }
 
     if(headsOrTails === 1) {
         coinBox.className = 'heads';
+        currenClass = 'heads';
         console.log(coinBox.className)
     }
+
+    setTimeout(() => {
+        coinBox.classList.remove(currenClass);
+    }, 5000);
 })
 
 btnReset.addEventListener('click', () => {
